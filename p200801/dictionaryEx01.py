a093e8dc41b26313c57d96b8b4b194e32093a87a
seyoungoh <osy8108@gmail.com>
2020-08-01 17:46:16 +0900

    test

---
+++ p200801/dictionaryEx01.py
@@ -0,0 +1,23 @@
+#딕셔너리
+# : 하나의 데이터가 키:값쌍으로 이루어짐
+# : 저장순서를 유지하지 않음 -> 인덱스 사용할 수 없음
+# : 키를 통해 값에 접근
+# : 여러 개의 데이터(요소)를 {}로 묶어서 관리
+# dc = {key:value}
+dc = {'name': 'hong', 'addr': 'Seoul Gangnam', 'phone': '010-1111-1111'}
+print(len(dc))  #요소의 갯수
+print(dc['name'])   #키를 통해서 값을 찾아온다. 키는 중복해서 X 값은 중복 O/ 딕셔너리는 인덱스를 사용하지 않는다.
+dc['name'] = 'Kim'  #이미 존재하는 키면 값 변경
+print(dc)
+print(type(dc))
+
+dc['age'] = 24      #존재하지 않는 키면 값 추가
+print(dc)
+
+del dc['addr']
+print(dc)
+
+#키만 추출
+print('key :', dc.keys())
+#값만 추출
+print('value :', dc.values())
