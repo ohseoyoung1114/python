c470ba76d87c7315f25fc6e3efb1b1c6a31adae4
seyoungoh <osy8108@gmail.com>
2020-08-08 14:35:30 +0900

    14:35

---
+++ p200808/book103.py
@@ -0,0 +1,20 @@
+# p.103
+
+s1 = 'python'
+print(bool(s1))
+
+s2 = ''
+print(bool(s2))
+
+lt1 = [1,3]
+print(bool(lt1))
+
+lt2 = []
+print(bool(lt2))
+
+print(bool(100))
+print(bool(-1))
+print(bool(0))
+print(bool(None))
+
+print(type(True))
