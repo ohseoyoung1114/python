bc554f70db342e93d185243ea1b216f710d71966
seyoungoh <osy8108@gmail.com>
2020-08-08 16:17:34 +0900

    16:17

---
+++ p200808/ifEx01.py
@@ -0,0 +1,24 @@
+
+num = 20
+#
+# if 조건식:
+#     명령문
+if num>0:
+    #print('<<true area>>')
+    #num += 100
+    pass #실행하고자하는게 없을때 쓴다. 명령문을 안써주면 error!!
+elif num==0:
+    print('num is 0...')
+else:
+    print('<<False area>>')
+
+print('num :', num)
+
+
+fruit = ['apple', 'banana']
+
+if 'apple' in fruit:
+    print('yes')
+
+else:
+    print('no')
